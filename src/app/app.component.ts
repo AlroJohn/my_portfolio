f561ab002b5efec3d4c3c79de720380922eb4562
Alro John Mercado <alromercado08@gmail.com>
2024-06-12 13:26:46 +0800

    alro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, CommonModule]
 })
 export class AppComponent {
+
+  constructor(private elementRef: ElementRef) {}
+
   title = 'portfolio-app';
   myName = 'Alro John T. Mercado';
 
@@ -24,5 +28,17 @@ export class AppComponent {
     'assets/logos/html.png',
     'assets/logos/css.png',
     'assets/logos/tailwind.png',
-  ]
+  ];
+
+  message_logo = [
+    { path: 'assets/logos/github.png', link: 'https://github.com/AlroJohn', height: 30, width: 30 },
+    { path: 'assets/logos/linkedin.png', 
+    link: 'https://www.linkedin.com/in/alro-john-mercado-b676172ba/?trk=opento_sprofile_topcard', height: 37, width: 37 },
+    { path: 'assets/logos/gmail.png', link: 'mailto:alromercado08@gmail.com', height: 25, width: 25 },
+  ];
+
+  scrollToElement($element: HTMLElement): void {
+    $element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    provideAnimations(),
+  ],
 };
